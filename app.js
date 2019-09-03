918fc14f8ae51989c37245907554f516b161101b
CN105 <cn105@CN0105.local>
2019-09-03 16:57:16 +0100

    jump animation established with test button

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+
+const test = document.querySelector('#test')
+const testJump = document.querySelector('#testJump')
+
+
+
+
+
+
+
+
+
+test.addEventListener('click',()=>{
+    console.log('test__')
+    testJump.classList.toggle("jump")
+
+})
