9fd76ae90302f6725fdaa2b567d0ef08ea066bde
CN105 <cn105@CN0105.local>
2019-09-04 10:45:37 +0100

    added random jumping

--- app.js
+++ app.js
@@ -6,17 +6,50 @@ const test = document.querySelector('#test')
 const testJump = document.querySelector('#testJump')
 
 
-const mrBump = document.getElementsByClassName('mrBump')
+const mrBump = document.querySelectorAll('.mrbump')
 
 
 
-mrBump[0].addEventListener('click',()=>{
-    mrBump[0].toggle("jump")
-})
+mrBumpStuff = {
+    randBump(){
+        let r = Math.floor((Math.random()* 6))
+        console.log(`r --> ${r}`)
+        return r},
+    jumpBump(i){
+        mrBump[i].classList.add("jump")
+        mrBump[i].addEventListener("animationend", ()=>{
+            mrBump[i].classList.remove("jump")
+        })
+
+    }
+
+}
+    
+
+const mainGame = () => { 
+
+    /**  */
+
+    /**  keep jumping **/
+    setInterval(()=>{
+        mrBumpStuff.jumpBump(mrBumpStuff.randBump())
+    }, 1000);
+}
+
+
+/*************  MAIN *******/
+mainGame()
+
+
+
+
+
+
+
 
 
 // test.addEventListener('click',()=>{
 //     console.log('test__')
-//     testJump.classList.toggle("jump")
+//     mrBumpStuff.jumpBump(mrBumpStuff.randBump())
 
 // })
